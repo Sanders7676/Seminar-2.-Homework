1cdd3f6ddc3dce4ce265727341ae245646b541fb
Sanders76 <sanders76@list.ru>
2022-11-26 23:45:30 +0300

    Create folder Task_2

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random rand = new Random();
+int number = rand.Next(1, 8);
+Console.WriteLine($"{number}-й день недели");
+int div = number / 10
