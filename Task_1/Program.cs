84c113e8fd2813f0bb8d3d8033f5c56efede9bac
Sanders76 <sanders76@list.ru>
2022-11-27 00:26:50 +0300

    Added code to Task_1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,4 +1,12 @@
-﻿Random rand = new Random();
-int number = rand.Next(1, 8);
-Console.WriteLine($"{number}-й день недели");
-int div = number / 10
+﻿// Задача № 1
+// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Random rand = new Random();
+
+int number = rand.Next(100, 1000);
+
+Console.WriteLine($"Наше число - {number}.");
+
+int div = (number / 10) % 10;
+
+Console.WriteLine($"Вторая цифра в этом числе - {div}.");
