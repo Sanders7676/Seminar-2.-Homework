c5c192110122d8363bee235bfc34f5b63ad8b9e1
Sanders76 <sanders76@list.ru>
2022-11-27 00:31:48 +0300

    Added the task to Nask_2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача № 2
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
