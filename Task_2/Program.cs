5e7d4d4f3a5fecf2a42f99c32e72c2ff4fb11c88
Sanders76 <sanders76@list.ru>
2022-11-29 00:45:34 +0300

    Added code to Task_2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -5,29 +5,43 @@ Console.Write("Введите число: ");
 
 bool isParsed = int.TryParse(Console.ReadLine(), out int number);
 
-//if (!isParsed)
-//{
-// Console.WriteLine("Число введено неверно");
-// return;
-//}
-
-if (number > 0)
+if (!isParsed)
 {
-    int discharge = 10;
+Console.WriteLine("Число введено неверно");
+return;
+}
 
-    int div = number / discharge;
-
-    while (div > 1000)
+if (number >= 0)
+{
+    if (number < 100)
     {
-        discharge = discharge * 10;
+        Console.WriteLine("В этом числе нет третьего знака!");
     }
+    else
+    {
+        int discharge = 10;
 
-    int result = div % 10;
+        if (number >= 1000)
+        {
+            while ((number / discharge) >= 1000)
+            {
+                discharge = discharge * 10;
+            }
+            
+            int result = (number / discharge) % 10;
 
-    Console.WriteLine($"Третья цифра в данном числе это {result}");
+            Console.WriteLine($"Третья цифра в данном числе это {result}");
+        }
+        else
+        {
+            int result = number % 10;
 
+            Console.WriteLine($"Третья цифра в данном числе это {result}");
+        }
+    }
 }
 else
 {
     Console.WriteLine("Это число меньше нуля.");
+   
 }
